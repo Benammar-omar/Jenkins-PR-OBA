7167c6bd8ba5dc447e7e55305cde1b31fd901b87
Benammar-omar <144702417+Benammar-omar@users.noreply.github.com>
2023-12-06 16:36:03 +0100

    01

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = { "stepsDefinition" }, monochrome = true, plugin = {
 		"pretty", "html:target/cucumber-reports.html","json:target/rapport.json",
-		"junit:target/cucumber-results.xml" }, tags = "@Login")
+		"junit:target/cucumber-results.xml" }, tags = "@Login or @displayCandidatures")
 public class TestRunner {
 
 }
